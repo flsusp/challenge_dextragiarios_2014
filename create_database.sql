c4286a8934412f6a90d28803bf96e1e4cb919c94
Matheus Martins do Rego <matheus.martins@dls107.lab.dextra.com.br>
2014-01-23 16:21:47 -0200

    Default da coluna 0

--- create_database.sql
+++ create_database.sql
@@ -4,10 +4,9 @@ create database challenge owner challenge_user;
 
 create table account (id serial primary key, nome varchar(30));
 
-create table contas (id serial primary key, idAccount int references account(id), valorRelativo decimal not null);
+create table contas (id serial primary key, idAccount int references account(id), valorRelativo decimal not null default 0);
 
 create table product (id serial primary key, stock bigint not null default 0, price decimal not null);
-
 
 
 create table negative_balance (account_id int not null references account(id), balance decimal not null, time timestamp not null);
