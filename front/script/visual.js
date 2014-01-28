0265094454d2d79a0d7255f45ab86fcf7e3425db
Guilherme Machado de Figueiredo Murari <guilherme.murari@dls91.lab.dextra.com.br>
2014-01-28 13:24:34 -0200

    Barra de pesquisa

--- front/script/visual.js
+++ front/script/visual.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+
+	//Efeito da nav-bar:
 	$("#nav-wrapper").hide();
     $(window).scroll(function(){
         if($(window).scrollTop() >  $(".nav-header").outerHeight()) {
@@ -7,7 +9,22 @@ $(document).ready(function(){
         	$("#nav-wrapper").slideUp();
         }
     });
+
+    //Limpar campo da pesquisa no topo:
+    var topo = $("#topo-busca").val();
     $("#topo-busca").focus(function() {
-    	$("#topo-busca").value = "";
+    	$("#topo-busca").val("");
+    });
+    $("#topo-busca").focusout(function() {
+    	$("#topo-busca").val(topo);
+    });
+
+    //Limpar campo da pesquisa no topo:
+    var nav = $("#nav-busca").val();
+    $("#nav-busca").focus(function() {
+    	$("#nav-busca").val("");
+    });
+    $("#nav-busca").focusout(function() {
+    	$("#nav-busca").val(nav);
     });
 })
